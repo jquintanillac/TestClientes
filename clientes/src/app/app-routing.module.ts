1a989b82432b8e44e034fdd70df18749a0987562
jquintanillac <juanqcalderon@gmail.com>
2022-02-08 22:56:22 -0500

    ListarClientes

--- clientes/src/app/app-routing.module.ts
+++ clientes/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateClientesComponent } from './Components/create-clientes/create-clientes.component';
+import { ListClientesComponent } from './Components/list-clientes/list-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-clientes', pathMatch: 'full' },  
+  { path: 'list-clientes', component: ListClientesComponent },
+  { path: 'create-clientes', component: CreateClientesComponent },
+  { path: '**', redirectTo: 'list-clientes', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
